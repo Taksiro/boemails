19fcaa99e6490e0fab43ac548320183521e46e6b
CLTaksiro <44567384+CLTaksiro@users.noreply.github.com>
2024-09-21 08:17:54 +0800

    Update app.js

--- VMBC-notes-main/CheatCODE/BO_Notes/app.js
+++ VMBC-notes-main/CheatCODE/BO_Notes/app.js
@@ -1,7 +1,7 @@
 var CN = document.getElementById('CN');
 var NoteArray = [];
 var checkboxes = document.querySelectorAll('.form-check-input');
-for(var checkbox of checkboxes) {
+/*for(var checkbox of checkboxes) {
     checkbox.addEventListener('click',function() {
         if (this.checked == true) {
         	NoteArray.push(this.value);
@@ -12,20 +12,29 @@ for(var checkbox of checkboxes) {
        }
    })
 }
-
+*/
+var gridp = document.getElementById("gridp"); 
+for(var cb of checkboxes) {
+    cb.addEventListener('click',function() {
+        if (this.checked == true) {
+        	NoteArray.push(this.value);
+        	gridp.innerHTML = NoteArray.join('/');
+        } else {
+        	NoteArray = NoteArray.filter(e => e !== this.value);
+					gridp.innerHTML = NoteArray.join('/');
+       }
+   })
+}
 function QAcopy(){
   var copyText = document.getElementById("CN");
   copyText.select();
   copyText.setSelectionRange(0, 99999);
   navigator.clipboard.writeText(copyText.value);
 }
-
-
-
-function AP() {
+/*function AP() {
 var checkBox = document.getElementById('AP');
 var n = document.getElementById('ADS');
-var t = document.getElementById('NN');
+var t = document.getElementById('listnote');
 const k = ["Program proof valid","DOB match","Not expired","FN match","LN match","Readable"];
 let f = k.length;
 let a = " ";
@@ -37,8 +46,186 @@ let a = " ";
  } else {
      t.value = "";
   }
+}*/
+
+const items = {
+  AP:['Program proof valid', 'DOB match', 'Not expired', 'FN match', 'LN match', 'Readable'],
+  DP:['Borders not shown', 'Purchase receipt incomplete', 'Purchase receipt declined', 'Purchase receipt expired', 'Name interchanged', 'FN is mismatch', 'LN is mismatch', 'DOB is mismatch', 'Address on proof does not match the address registered', 'Expired proof', 'Key information erased or modified'], 
+  AD:['Program proof valid', 'Address match', 'Not expired', 'FN match', 'LN match', 'Readable'], 
+  AA:['Program proof valid', 'Address match', 'Not expired', 'FN match', 'LN match', 'Readable'], 
+  AFAP:['Address match', 'Program proof valid', 'Not expired', 'FN match', 'LN match', 'Readable'],  
+  AI:['FN match', 'LN match', 'Income proof valid', 'Income amount does not exceed the limit', 'Readable'], 
+  ASP:['FN match', 'LN match', 'SSN proof valid', 'Income amount does not exceed the limit', 'Four digit number match', 'Readable'], 
+  ADSP:['FN match', 'LN match', 'Dependent SSN proof valid', 'Four digit number match', 'Without signature accepted under 18', 'Readable'],  
+  ADDP:['FN match', 'LN match', 'Dependent DOB proof valid', 'Readable'],  
+  DD:['Borders not shown', 'Name interchanged', 'FN is mismatch', 'LN is mismatch', 'DOB is mismatch', 'Letter no longer valid', 'Expired proof', 'Not allowed type of proof', 'Key information erased or modified'],  
+  DAP:['Address on proof does not match the address registered', 'Name interchanged', 'FN is mismatch', 'LN is mismatch', 'Letter no longer valid', 'Not allowed type of proof', 'Key information erased or modified'],  
+  DFAP:['Address on proof does not match the address registered', 'Name interchanged', 'FN is mismatch', 'LN is mismatch', 'Letter no longer valid', 'Not allowed type of proof', 'Expired proof older than 90 days', 'Key information erased or modified', 'Rental letter not allowed'],  
+  DI:['Name interchanged', 'FN is mismatch', 'LN is mismatch', 'Letter no longer valid', 'Income amount exceeds the limit', 'Expired proof', 'Key information erased or modified'], 
+  DSP:['FN is mismatch', 'LN is mismatch', 'Name interchanged', 'Missing signature', 'Old SSN card no longer valid', 'Type of proof not allowed', 'Four digit number on proof does not match the number registered', 'Key information erased or modified'], 
+  DDSP:['FN is mismatch', 'LN is mismatch', 'Name interchanged', 'Four digit number on proof does not match the number registered', 'Dependent SSN invalid', 'Type of proof not allowed', 'Key information erased or modified'],  
+  DDD:['FN is mismatch', 'LN is mismatch', 'Name interchanged', 'Dependent DOB invalid', 'Key information erased or modified']
 }
 
+function handleCheckbox(checkbox){
+  const lnt = document.getElementById('listnote');
+  const cln = document.getElementById('cnts');
+  let selecteditems = [];
+  switch (checkbox.value){
+    case 'Approved - Program Proof':
+      selecteditems.push(...items.AP);
+      break;
+    case 'Disapproved - Program Proof':
+      selecteditems.push(...items.DP);
+      break;
+    case 'Approved - DOB Proof':
+      selecteditems.push(...items.AD);
+      break;
+    case 'Approved - Address Proof':
+      selecteditems = items.AA;
+      break;
+    case 'Approved - FAP':
+      selecteditems = items.AFAP;
+      break;
+    case 'Approved - Income Proof':
+      selecteditems = items.AI;
+      break;
+    case 'Approved - SSN Proof':
+      selecteditems = items.ASP;
+      break;
+    case 'Approved - Dependent SSN Proof':
+      selecteditems = items.ADSP;
+      break;
+    case 'Approved - Dependent DOB Proof':
+      selecteditems = items.ADDP;
+      break;
+    case 'Disapproved - DOB Proof':
+      selecteditems = items.DD;
+      break;
+    case 'Disapproved - Address Proof':
+      selecteditems = items.DAP;
+      break;
+    case 'Disapproved - FAP':
+      selecteditems = items.DFAP;
+      break;
+    case 'Disapproved - Income Proof':
+      selecteditems = items.DI;
+      break;
+    case 'Disapproved - SSN Proof':
+      selecteditems = items.DSP;
+      break;
+    case 'Disapproved - Dependent SSN Proof':
+      selecteditems = items.DDSP;
+      break;
+    case 'Disapproved - Dependent DOB Proof':
+      selecteditems = items.DDD;
+      break;
+    default:
+      console.log('Unknown Action');
+      return;
+  }
+
+  if(checkbox.checked){
+    selecteditems.forEach(itemz =>{
+      const button = `${checkbox.value}-${itemz.replace(/\s+/g, '-')}`;
+      if(!document.getElementById(button)){
+        const lstbutton = document.createElement('button');  
+        lstbutton.className = 'btn btn-outline-primary';
+        lstbutton.textContent = itemz;
+        lstbutton.id = button;
+        lstbutton.style.margin = '4px';
+        lstbutton.addEventListener('click', () => {
+          if(cln.children.length > 0 ){
+            const separator = document.createTextNode(', ');
+            cln.appendChild(separator);
+          }
+          const nnts = `${itemz.replace(/\s+/g, ' ')}`;
+          const lc = document.createElement('span');
+          lstbutton.hidden = true;
+          lc.textContent = nnts;
+          lc.id = nnts;
+          lc.style.cursor = 'pointer';
+          cln.appendChild(lc);
+          lc.addEventListener('click' , () =>{
+            lstbutton.hidden = false;
+            cln.removeChild(lc);
+          if(cln.lastChild && cln.lastChild.nodeType === Node.TEXT_NODE){
+            cln.removeChild(cln.lastChild);
+          }
+          if(cln.firstChild && cln.firstChild.nodeType === Node.TEXT_NODE){
+            cln.removeChild(cln.firstChild);
+          }
+          //alert(`You clicked on ${itemz}!`);
+          });
+        });
+        lnt.appendChild(lstbutton);
+      }
+    });
+  }else{
+    selecteditems.forEach(itemzz => {
+      const nnls = `${itemzz.replace(/\s+/g, ' ')}`;
+      const buttonr = `${checkbox.value}-${itemzz.replace(/\s+/g, '-')}`;
+      const remn = document.getElementById(nnls);
+      const buttonrr = document.getElementById(buttonr);
+      if(buttonrr){
+        lnt.removeChild(buttonrr);
+      }
+      if(remn){
+        cln.removeChild(remn);
+        while (cln.firstChild && cln.firstChild.nodeType === Node.TEXT_NODE){
+          cln.removeChild(cln.firstChild);
+        }
+        while(cln.lastChild && cln.lastChild.nodeType === Node.TEXT_NODE){
+          cln.removeChild(cln.lastChild);
+        }
+      }
+    });
+  }
+}
+
+function AsssP() {
+  const cl = document.getElementById('AP');
+  const lnt = document.getElementById('listnote');
+  const cln = document.getElementById('cnts');
+  var listarray = [];
+      listarray.push("Program proof valid","DOB match","Not expired","FN match","LN match","Readable");
+  lnt.innerHTML = '';
+  if (cl.checked){
+    listarray = listarray.filter(e => e !== this.listarray);
+    listarray.forEach(lns => {
+      const listnotes = document.createElement('li');
+      listnotes.textContent = lns;
+      listnotes.addEventListener('click', () => {
+        const lst = document.createElement('span');
+        listnotes.hidden = true;
+        if (cln.children.length > 0) {
+          const comma = document.createTextNode(' , ');
+          cln.appendChild(comma);
+        }
+        lst.textContent = lns;
+        cln.appendChild(lst);
+        /*Alert(`${lns}`);*/
+        lst.addEventListener('click', () =>{
+          listnotes.hidden = false;
+          cln.removeChild(lst);
+          if(cln.lastChild && cln.lastChild.nodeType === Node.TEXT_NODE){
+            cln.removeChild(cln.lastChild);
+          }
+          if(cln.firstChild && cln.firstChild.nodeType === Node.TEXT_NODE){
+            cln.removeChild(cln.firstChild);
+          }
+        });
+      });
+      lnt.appendChild(listnotes);
+    });
+  }else{
+    
+   while (cln.firstChild) {
+      cln.removeChild(cln.firstChild);
+    }
+  }
+  cl.addEventListener('change', AP);
+}
 function AD() {
 var checkBox = document.getElementById('AD');
 var n = document.getElementById('ADS');
@@ -55,7 +242,6 @@ let a = " ";
      t.value = "";
   }
 }
-
 function AA() {
 var checkBox = document.getElementById('AA');
 var n = document.getElementById('AAS');
@@ -72,7 +258,6 @@ let a = " ";
      t.value = "";
   }
 }
-
 function AA() {
 var checkBox = document.getElementById('AA');
 var n = document.getElementById('AAS');
@@ -89,7 +274,6 @@ var n = document.getElementById('AAS');
      t.value = "";
   }
 }
-
 function AFAP() {
 var checkBox = document.getElementById('AFAP');
 var n = document.getElementById('AFS');
@@ -106,7 +290,6 @@ let a = " ";
      t.value = "";
   }
 }
-
 function AI() {
 var checkBox = document.getElementById('AI');
 var n = document.getElementById('AIS');
@@ -123,7 +306,6 @@ let a = " ";
      t.value = "";
   }
 }
-
 function ASP() {
 var checkBox = document.getElementById('ASP');
 var n = document.getElementById('ASPS');
@@ -140,7 +322,6 @@ let a = " ";
      t.value = "";
   }
 }
-
 function ADSP() {
 var checkBox = document.getElementById('ADSP');
 var n = document.getElementById('ADSS');
@@ -157,7 +338,6 @@ let a = " ";
      t.value = "";
   }
 }
-
 function ADDP() {
 var checkBox = document.getElementById('ADDP');
 var n = document.getElementById('ADDS');
@@ -174,8 +354,48 @@ let a = " ";
      t.value = "";
   }
 }
-
-function DP() {
+function DPsss(){
+  const cl = document.getElementById('DP');
+  const lnt = document.getElementById('listnote');
+  const cln = document.getElementById('cnts');
+  var ln = [];
+  lnt.innerHTML = '';
+  if (cl.checked){
+    ln.push("Borders not shown","Purchase receipt incomplete","Purchase receipt declined","Purchase receipt expired","Name interchanged","FN is mismatch","LN is mismatch","DOB is mismatch","Address on proof does not match the address registered","Expired proof","Key information erased or modified");
+    ln.forEach(lns => {
+      const listnotes = document.createElement('li');
+      listnotes.textContent = lns;
+      listnotes.addEventListener('click', () => {
+        const lst = document.createElement('span');
+        listnotes.hidden = true;
+        if (cln.children.length > 0) {
+          const comma = document.createTextNode(' , ');
+          cln.appendChild(comma);
+        }
+        lst.textContent = lns;
+        cln.appendChild(lst);
+        /*Alert(`${lns}`);*/
+        lst.addEventListener('click', () =>{
+          listnotes.hidden = false;
+          cln.removeChild(lst);
+          if(cln.lastChild && cln.lastChild.nodeType === Node.TEXT_NODE){
+            cln.removeChild(cln.lastChild);
+          }
+          if(cln.firstChild && cln.firstChild.nodeType === Node.TEXT_NODE){
+            cln.removeChild(cln.firstChild);
+          }
+        });
+      });
+      lnt.appendChild(listnotes);
+    });
+  }else{
+   /* while (cln.firstChild) {
+      cln.removeChild(cln.firstChild);
+    }*/
+  }
+  cl.addEventListener('change', DP);
+}
+/* function DP() {
 var checkBox = document.getElementById('DP');
 var n = document.getElementById('DPS');
 var t = document.getElementById('NN');
@@ -190,8 +410,7 @@ let a = " ";
   } else {
      t.value = "";
   }
-}
-
+}*/
 function DD() {
 var checkBox = document.getElementById('DD');
 var n = document.getElementById('DDS');
@@ -208,7 +427,6 @@ let a = " ";
      t.value = "";
   }
 }
-
 function DAP() {
 var checkBox = document.getElementById('DAP');
 var n = document.getElementById('DAPS');
@@ -225,7 +443,6 @@ let a = " ";
      t.value = "";
   }
 }
-
 function DFAP() {
 var checkBox = document.getElementById('DFAP');
 var n = document.getElementById('DFAPS');
@@ -242,7 +459,6 @@ let a = " ";
      t.value = "";
   }
 }
-
 function DI() {
 var checkBox = document.getElementById('DI');
 var n = document.getElementById('DIS');
@@ -259,7 +475,6 @@ let a = " ";
      t.value = "";
   }
 }
-
 function DSP() {
 var checkBox = document.getElementById('DSP');
 var n = document.getElementById('DSPS');
@@ -276,7 +491,6 @@ let a = " ";
      t.value = "";
   }
 }
-
 function DDSP() {
 var checkBox = document.getElementById('DDSP');
 var n = document.getElementById('DDSPS');
@@ -293,7 +507,6 @@ let a = " ";
      t.value = "";
   }
 }
-
 function DDD() {
 var checkBox = document.getElementById('DDD');
 var n = document.getElementById('DDDS');
@@ -310,19 +523,17 @@ let a = " ";
      t.value = "";
   }
 }
-
 function deSelect(){  
 var c = document.getElementsByName('chk');
 let txtbox = document.getElementById('CN');
-const note= document.getElementById('NN');
+const note= document.getElementById('cnts');
 for(var i = 0; i<c.length; i++){
-    c[i].checked = false;
-    txtbox.value = '';
-    window.location.reload(true);
-    note.value = '';
+    checkboxes[i].checked = false;
+   // window.location.reload(true);
+ 
+  
 
 }
-
 //var a= document.getElementById('AP');
 //var b= document.getElementById('AD');
 //var c= document.getElementById('AA');
@@ -373,7 +584,6 @@ for(var i = 0; i<c.length; i++){
 //q.parentNode.style.fontWeight = 'normal';
 
 }
-
 function startTime() {
 const today = new Date();
 let h = today.getHours();
@@ -384,7 +594,6 @@ s = checkTime(s);
   document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
   setTimeout(startTime, 1000);
 }
-
 function checkTime(i) {
   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
   return i;
