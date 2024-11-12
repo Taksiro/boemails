e5a8eda208c6d4a0a89543e0c444fe21f0e8e0b8
CLTaksiro <44567384+CLTaksiro@users.noreply.github.com>
2024-11-12 11:36:29 +0800

    Update app.js

--- VMBC-notes-main/CheatCODE/BO_Notes/app.js
+++ VMBC-notes-main/CheatCODE/BO_Notes/app.js
@@ -616,11 +616,29 @@ function formatText(command) {
   }
 }
 
-function setCookie(area, value, expiryDate) {
-  let val = document.getElementById('floatingTextarea');
-  var d = new Date();
-  d.setTime(d.getTime() + (expiryDate*24*60*60*1000));
-  var expires = "expires="+ d.toUTCString();
-val = document.cookie = area + "=" + value + "; " + expires + "; path=/";
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 
